0373fa6d350a4abace1e37afa525c857efed2b43
Huy <106498928+Vanhuyne@users.noreply.github.com>
2024-07-23 16:02:23 +0700

    add rating for froduct

---
+++ ecommerce/src/main/java/com/huy/ecommerce/controller/RatingController.java
@@ -0,0 +1,11 @@
+package com.huy.ecommerce.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/ratings")
+public class RatingController {
+}
